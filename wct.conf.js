0ce1a9a7725fd28e450c7157b817099859d8e623
Nicolas Corral <nico.corral@gmail.com>
2018-01-05 14:46:29 -0500

    initial commit

---
+++ wct.conf.js
@@ -0,0 +1,26 @@
+module.exports = {
+  suites: ['test/index.html'],
+  plugins: {
+    local: {
+      browsers: ['chrome']
+    },
+    sauce: {
+      disabled: true
+    },
+    istanbub: {
+      dir: './coverage',
+      reporters: ['text-summary', 'lcov'],
+      include: [
+        '**/*.html'
+      ]
+      // thresholds: {
+      //   global: {
+      //     statements: 80,
+      //     branches: 80,
+      //     functions: 90,
+      //     lines: 80
+      //   }
+      // }
+    }
+  }
+}
